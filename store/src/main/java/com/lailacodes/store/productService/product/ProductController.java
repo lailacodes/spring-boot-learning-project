98534567503df3bd1fcde5bb4141b795b12872d6
Laila <93746327+lailacodes@users.noreply.github.com>
2026-01-12 09:29:20 -0500

    Made updates to product service overall

--- store/src/main/java/com/lailacodes/store/productService/product/ProductController.java
+++ store/src/main/java/com/lailacodes/store/productService/product/ProductController.java
@@ -1,8 +1,27 @@
 package com.lailacodes.store.productService.product;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<ProductView> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{name}")
+    public ProductView getProductByName(@PathVariable String name) {
+        return productService.getProductByName(name)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
 }
--- store/src/main/java/com/lailacodes/store/productService/product/ProductRepository.java
+++
@@ -1,4 +0,0 @@
-package com.lailacodes.store.productService.product;
-
-public class ProductRepository {
-}
